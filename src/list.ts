5af6a09c78bcd034d084844e2bd9c6b6af4ed60f
Mateusz Haligowski <matehal@preemo.io>
2023-08-16 20:45:19 -0700

    first working version

---
+++ src/list.ts
@@ -0,0 +1,22 @@
+import { Configuration, ModelsApi } from "@gradientai/nodejs-sdk";
+
+async function main() {
+  const configuration = new Configuration({
+    accessToken: process.env.GRADIENT_API_KEY,
+    basePath: process.env.GRADIENT_API_URL,
+  });
+
+  const modelsApi = new ModelsApi(configuration);
+  const models = await modelsApi.listModels({
+    onlyBase: "false",
+    xPreemoWorkspaceId: process.env.GRADIENT_WORKSPACE_ID!,
+  });
+
+  if (models.status !== 200) {
+    throw new Error("Failed to list models");
+  }
+
+  console.log(models.data);
+}
+
+main().catch(console.error).then(console.log);
