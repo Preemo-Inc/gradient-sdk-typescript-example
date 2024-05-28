55a24a333d76239af7f14b93e701dbeea4963cd6
GradientBot <it-admin@gradient.ai>
2024-05-28 15:09:04 -0700

    Project import generated by Copybara.
    GitOrigin-RevId: e366ae53dd48e76d702007ac3c1866e7c3d0caa9

--- src/rag.ts
+++ src/rag.ts
@@ -3,9 +3,44 @@ import { Gradient } from "@gradientai/nodejs-sdk";
 const main = async () => {
   const gradient = new Gradient({});
 
-  const ragCollection = await gradient.createRagCollection({
+  const sentenceWindowNodeParserRagCollection =
+    await gradient.createRagCollection({
+      filepaths: ["resources/Lorem_Ipsum.pdf"],
+      name: "RAG Collection with Sentence Window Node Parser",
+      parser: {
+        chunkSize: 1024,
+        chunkOverlap: 20,
+        parserType: "sentenceWindowNodeParser",
+        windowSize: 3,
+      },
+      slug: "bge-large",
+    });
+  console.log(
+    `Created RAG collection with id: ${sentenceWindowNodeParserRagCollection.id}`
+  );
+
+  console.log(
+    `RAG collection: ${JSON.stringify(
+      sentenceWindowNodeParserRagCollection,
+      null,
+      2
+    )}`
+  );
+
+  await sentenceWindowNodeParserRagCollection.addFiles({
+    filepaths: ["resources/Life_Kit.mp3"],
+  });
+  console.log(
+    `RAG collection files: ${JSON.stringify(
+      sentenceWindowNodeParserRagCollection.files,
+      null,
+      2
+    )}`
+  );
+
+  const simpleNodeParserRagCollection = await gradient.createRagCollection({
     filepaths: ["resources/Lorem_Ipsum.pdf"],
-    name: "My RAG Collection",
+    name: "RAG Collection with Simple Node Parser",
     parser: {
       chunkSize: 1024,
       chunkOverlap: 20,
@@ -13,18 +48,27 @@ const main = async () => {
     },
     slug: "bge-large",
   });
-  console.log(`Created RAG collection with id: ${ragCollection.id}`);
+  console.log(
+    `Created RAG collection with id: ${simpleNodeParserRagCollection.id}`
+  );
 
-  console.log(`RAG collection: ${JSON.stringify(ragCollection, null, 2)}`);
+  console.log(
+    `RAG collection: ${JSON.stringify(simpleNodeParserRagCollection, null, 2)}`
+  );
 
-  await ragCollection.addFiles({
+  await simpleNodeParserRagCollection.addFiles({
     filepaths: ["resources/Life_Kit.mp3"],
   });
   console.log(
-    `RAG collection files: ${JSON.stringify(ragCollection.files, null, 2)}`
+    `RAG collection files: ${JSON.stringify(
+      simpleNodeParserRagCollection.files,
+      null,
+      2
+    )}`
   );
 
-  await ragCollection.delete({});
+  await sentenceWindowNodeParserRagCollection.delete({});
+  await simpleNodeParserRagCollection.delete({});
 };
 
 main()
