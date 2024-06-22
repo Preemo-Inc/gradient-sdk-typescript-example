bd2e1ae7e87022cad80ac76ddde6da9fb71bfff0
GradientBot <it-admin@gradient.ai>
2024-06-21 17:55:56 -0700

    Project import generated by Copybara.
    GitOrigin-RevId: 172aef9cc4436b863645312cce8e7fe386d99225

--- src/rag.ts
+++ src/rag.ts
@@ -1,74 +1,87 @@
 import { Gradient } from "@gradientai/nodejs-sdk";
+import {
+  FileChunker,
+  MeaningBasedChunker,
+  NormalChunker,
+  RagChunker,
+  SentenceWithContextChunker,
+} from "@gradientai/nodejs-sdk/dist/cjs/rag/paramTypes";
+import { RagCollection } from "@gradientai/nodejs-sdk/dist/cjs/rag/ragCollection";
+
+const FILE_CHUNKER_CONFIG: FileChunker = {
+  chunkerType: "fileChunker" as const,
+};
+
+const MEANING_BASED_CHUNKER_CONFIG: MeaningBasedChunker = {
+  chunkerType: "meaningBasedChunker" as const,
+  overlap: 20,
+  sentenceGroupLength: 1,
+  similiarityPercentThreshold: 95,
+  size: 1024,
+};
+
+const NORMAL_CHUNKER_CONFIG: NormalChunker = {
+  chunkerType: "normalChunker" as const,
+  overlap: 20,
+  size: 1024,
+};
+
+const SENTENCE_WITH_CONTEXT_CHUNKER_CONFIG: SentenceWithContextChunker = {
+  chunkerType: "sentenceWithContextChunker" as const,
+  contextSentences: 3,
+  overlap: 20,
+  size: 1024,
+};
+
+const buildRagCollection = async (
+  gradient: Gradient,
+  chunkerConfig: RagChunker
+): Promise<RagCollection> => {
+  const ragCollection = await gradient.createRagCollection({
+    filepaths: ["resources/Lorem_Ipsum.pdf"],
+    name: `RAG Collection with ${chunkerConfig.chunkerType} chunker`,
+    chunker: chunkerConfig,
+    slug: "bge-large",
+  });
+  console.log(`Created RAG collection with id: ${ragCollection.id}`);
+  console.log(`Chunker: ${JSON.stringify(ragCollection.chunker)}`);
+
+  await ragCollection.addFiles({
+    filepaths: ["resources/Life_Kit.mp3"],
+  });
+  console.log(
+    `RAG collection files: ${JSON.stringify(ragCollection.files, null, 2)}`
+  );
+  return ragCollection;
+};
 
 const main = async () => {
   const gradient = new Gradient({});
 
-  const sentenceWindowNodeParserRagCollection =
-    await gradient.createRagCollection({
-      filepaths: ["resources/Lorem_Ipsum.pdf"],
-      name: "RAG Collection with Sentence Window Node Parser",
-      parser: {
-        chunkSize: 1024,
-        chunkOverlap: 20,
-        parserType: "sentenceWindowNodeParser",
-        windowSize: 3,
-      },
-      slug: "bge-large",
-    });
-  console.log(
-    `Created RAG collection with id: ${sentenceWindowNodeParserRagCollection.id}`
+  const fileChunkerRagCollection = await buildRagCollection(
+    gradient,
+    FILE_CHUNKER_CONFIG
   );
 
-  console.log(
-    `RAG collection: ${JSON.stringify(
-      sentenceWindowNodeParserRagCollection,
-      null,
-      2
-    )}`
+  const meaningBasedChunkerRagCollection = await buildRagCollection(
+    gradient,
+    MEANING_BASED_CHUNKER_CONFIG
   );
 
-  await sentenceWindowNodeParserRagCollection.addFiles({
-    filepaths: ["resources/Life_Kit.mp3"],
-  });
-  console.log(
-    `RAG collection files: ${JSON.stringify(
-      sentenceWindowNodeParserRagCollection.files,
-      null,
-      2
-    )}`
+  const normalChunkerRagCollection = await buildRagCollection(
+    gradient,
+    NORMAL_CHUNKER_CONFIG
   );
 
-  const simpleNodeParserRagCollection = await gradient.createRagCollection({
-    filepaths: ["resources/Lorem_Ipsum.pdf"],
-    name: "RAG Collection with Simple Node Parser",
-    parser: {
-      chunkSize: 1024,
-      chunkOverlap: 20,
-      parserType: "simpleNodeParser",
-    },
-    slug: "bge-large",
-  });
-  console.log(
-    `Created RAG collection with id: ${simpleNodeParserRagCollection.id}`
+  const sentenceWithContextChunkerRagCollection = await buildRagCollection(
+    gradient,
+    SENTENCE_WITH_CONTEXT_CHUNKER_CONFIG
   );
 
-  console.log(
-    `RAG collection: ${JSON.stringify(simpleNodeParserRagCollection, null, 2)}`
-  );
-
-  await simpleNodeParserRagCollection.addFiles({
-    filepaths: ["resources/Life_Kit.mp3"],
-  });
-  console.log(
-    `RAG collection files: ${JSON.stringify(
-      simpleNodeParserRagCollection.files,
-      null,
-      2
-    )}`
-  );
-
-  await sentenceWindowNodeParserRagCollection.delete({});
-  await simpleNodeParserRagCollection.delete({});
+  await fileChunkerRagCollection.delete({});
+  await meaningBasedChunkerRagCollection.delete({});
+  await normalChunkerRagCollection.delete({});
+  await sentenceWithContextChunkerRagCollection.delete({});
 };
 
 main()
